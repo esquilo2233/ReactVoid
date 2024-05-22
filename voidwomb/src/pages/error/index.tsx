fd1242a99a1528b251d1f1212e282c14c0c17c87
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2024-05-22 16:51:31 +0100

    .
    .

--- voidwomb/src/pages/error/index.tsx
+++ voidwomb/src/pages/error/index.tsx
@@ -1,5 +1,21 @@
-export default function ErrorPage() {
-    return (
-    <p>Sorry, something went wrong</p>
+// pages/auth/error.tsx
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const ErrorPage = () => {
+  const router = useRouter();
+  const { error } = router.query;
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded shadow-md">
+        <h2 className="text-2xl font-bold text-center">Error</h2>
+        <p className="text-center">
+          {error ? `Error: ${error}` : 'An unknown error occurred.'}
+        </p>
+      </div>
+    </div>
   );
-  }
+};
+
+export default ErrorPage;
