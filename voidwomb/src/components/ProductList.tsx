8972adb2d2dab2aef605a34d368d33177d693f3c
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2024-05-23 15:22:10 +0100

    s
    s

--- voidwomb/src/components/ProductList.tsx
+++ voidwomb/src/components/ProductList.tsx
@@ -36,8 +36,8 @@ const ProductList: React.FC<ProductListProps> = ({ products, onDelete }) => {
               </thead>
               <tbody>
               {products.map(product => (
-                  <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white" key={product.id}>
+                  <tr key={product.id} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                       </th>
                       <td className="px-6 py-4">
                         {product.name}
