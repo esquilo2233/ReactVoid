733a557455b8a612e5e6003d69348d8d1e441af3
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2024-05-23 15:30:03 +0100

    s
    s

--- voidwomb/src/components/ProductForm.tsx
+++ voidwomb/src/components/ProductForm.tsx
@@ -39,12 +39,13 @@ const ProductForm: React.FC<ProductFormProps> = ({ onSubmit, initialData = {} })
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Name</label>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+    
+    <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+      <div className="relative z-0 w-full mb-5 group">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+        <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" value={name} onChange={(e) => setName(e.target.value)} />
       </div>
-      <div>
+      <div className="relative z-0 w-full mb-5 group">
         <label>SKU</label>
         <input type="text" value={sku} onChange={(e) => setSku(e.target.value)} />
       </div>
