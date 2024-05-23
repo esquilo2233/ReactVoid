57dec89beed4dda4bfeec26077ebe6d515245cc8
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2024-05-23 15:17:36 +0100

    d
    d

--- voidwomb/src/components/ProductList.tsx
+++ voidwomb/src/components/ProductList.tsx
@@ -18,7 +18,7 @@ const ProductList: React.FC<ProductListProps> = ({ products, onDelete }) => {
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
                       <th scope="col" className="px-6 py-3">
-                          ID
+                          
                       </th>
                       <th scope="col" className="px-6 py-3">
                           Nome do Produto
@@ -38,7 +38,6 @@ const ProductList: React.FC<ProductListProps> = ({ products, onDelete }) => {
               {products.map(product => (
                   <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                       <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {product.id}
                       </th>
                       <td className="px-6 py-4">
                         {product.name}
