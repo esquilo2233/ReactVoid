b362392fa9fca0a28e991ce9cd1bf7308a017bc0
Lucas Cruz <124691704+ludvikcruz@users.noreply.github.com>
2024-05-24 10:42:13 +0100

    .

--- voidwomb/src/pages/contact/index.tsx
+++ voidwomb/src/pages/contact/index.tsx
@@ -21,7 +21,8 @@ voidwomb.band [at] gmail.com</p>
               <label htmlFor="message" className="block mb-2 text-sm font-medium text-white dark:text-gray-400">Your message</label>
               <textarea id="message"  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Write the text..."></textarea>
           </div>
-          <button type="submit" className="py-3 px-5 text-sm font-medium border-white text-center text-white rounded-lg bg-transparent sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+          <button type="submit" className="py-3 px-5 text-sm font-medium border border-white text-center text-white rounded-lg bg-transparent sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+
       </form>
   </div>
 </section>
