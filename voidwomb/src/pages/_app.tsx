74cc8a142b9c08881201f6e3e5fbbd7360ccf8e0
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2024-05-24 15:26:23 +0100

    .
    .

--- voidwomb/src/pages/_app.tsx
+++ voidwomb/src/pages/_app.tsx
@@ -1,40 +1,17 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-import ProductForm from '../components/ProductForm';
-import { addProduct } from '../services/productService';
+import React from 'react'
+import { SessionProvider } from 'next-auth/react'
+import RootLayout from '../components/RootLayout'
+import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
-const AddProductPage: React.FC = () => {
-  const router = useRouter();
-
-  const handleAddProduct = async (formData: FormData) => {
-    const productData = {
-      name: formData.get('name') as string,
-      sku: formData.get('sku') as string,
-      price: parseFloat(formData.get('price') as string),
-      totalStock: parseInt(formData.get('totalStock') as string),
-      description: formData.get('description') as string,
-      color: formData.get('color') as string,
-      totalSelled: 0,
-    };
-
-    const images = Array.from(formData.getAll('images')).map((image) => ({
-      imageUrl: URL.createObjectURL(image as File),
-    }));
-
-    const sizes = Array.from(formData.getAll('sizes')).map((size) =>
-      JSON.parse(size as string)
-    );
-
-    await addProduct(productData, images, sizes);
-    router.push('/adm/produtos');
-  };
-
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <h1>Add New Product</h1>
-      <ProductForm onSubmit={handleAddProduct} />
-    </div>
-  );
-};
+    <SessionProvider session={pageProps.session}>
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
+    </SessionProvider>
+  )
+}
 
-export default AddProductPage;
+export default MyApp
