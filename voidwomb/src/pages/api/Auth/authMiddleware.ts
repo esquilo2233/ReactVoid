a4a150148baa54e5dda45be7ac1c17734b1f44d4
Andre Carvalho <114291608+esquilo2233@users.noreply.github.com>
2024-06-05 11:05:01 +0100

    .
    .

--- voidwomb/src/pages/api/Auth/authMiddleware.ts
+++ voidwomb/src/pages/api/Auth/authMiddleware.ts
@@ -1,6 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { JWT } from 'next-auth/jwt';
+
+// Defina uma interface que estenda JwtPayload e inclua as propriedades que você espera no token
+interface CustomJwtPayload extends JwtPayload {
+  id: number;
+  email: string;
+  is_staff: boolean;
+}
+
+// Estenda a interface do NextApiRequest para incluir a propriedade user
+declare module 'next' {
+  interface NextApiRequest {
+    user?: CustomJwtPayload;
+  }
+}
 
 export async function authenticate(req: NextApiRequest, res: NextApiResponse, next: Function) {
   const authHeader = req.headers.authorization;
@@ -12,7 +26,7 @@ export async function authenticate(req: NextApiRequest, res: NextApiResponse, ne
   const token = authHeader.split(' ')[1];
 
   try {
-    const decoded = jwt.verify(token, process.env.NEXTAUTH_SECRET || 'supersecret') as JWT;
+    const decoded = jwt.verify(token, process.env.NEXTAUTH_SECRET || 'supersecret') as CustomJwtPayload;
     req.user = decoded;
     next();
   } catch (err) {
--- voidwomb/src/pages/store/index.tsx
+++ voidwomb/src/pages/store/index.tsx
@@ -1,43 +1,58 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import prisma from '../../utils/prisma'
 
-function Store() {
+export default async function Store() {
+
+
+  const products = await prisma.product.findMany();
+
+
   return (
     <div className="container mx-auto mt-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         <div className="w-full max-w-sm bg-transparent border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-4">
-          <Link href="/store/product">
-            <div className="p-8">
-              <Image
-                className="rounded-t-lg"
-                src="/img/Merch_FotosSA/T-ShirtRedSA_BackZoom.jpg"
-                alt="product image"
-                width={500}
-                height={500}
-              />
-            </div>
-          </Link>
-          <div className="px-5 pb-5">
-            <Link href="/store/product">
-              <h5 className="text-xl font-semibold tracking-tight text-white dark:text-white">
-                Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-              </h5>
-            </Link>
-            <div className="flex items-center justify-between">
-              <span className="text-3xl font-bold text-white dark:text-white">$599</span>
-              <Link
-                href="/store/product"
-                className="text-white bg-transparent hover:bg-transparent focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-transparent dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Add to cart
+        {
+          products.map((product) => (
+            <li key={product.id} className="product-item">
+              <Link href={`/store/product/${product.id}`}>
+                <div className="p-8">
+                  {/* <Image
+                    className="rounded-t-lg"
+                    src={}
+                    alt={`${product.name} image`}
+                    width={500}
+                    height={500}
+                  /> */}
+                </div>
               </Link>
-            </div>
-          </div>
+              <div className="px-5 pb-5">
+                <Link href={`/store/product/${product.id}`}>
+                  <h5 className="text-xl font-semibold tracking-tight text-white dark:text-white">
+                    {product.name}
+                  </h5>
+                </Link>
+                <div className="flex items-center justify-between">
+                  <span className="text-3xl font-bold text-white dark:text-white">
+                    ${product.price}
+                  </span>
+                  <Link
+                    href={`/store/product/${product.id}`}
+                    className="text-white bg-transparent hover:bg-transparent focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-transparent dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Add to cart
+                  </Link>
+                </div>
+              </div>
+            </li>
+          ))
+        }
+
+          
+            
         </div>
       </div>
     </div>
   );
 }
-
-export default Store;
